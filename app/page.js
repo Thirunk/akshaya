9876275c0ea60a85dccc8ddfb8178c07a1a23a6e
thiru <ask4thiru@gmail.com>
2024-12-14 08:52:00 +0530

    commit

--- app/page.js
+++ app/page.js
@@ -1,5 +1,22 @@
 import Image from "next/image";
+import bg from "@/public/bg.jpg";
 
-export default function Home() {
-  return <h1>Thiru&apos;s app</h1>;
+export default function Page() {
+  return (
+    <main className="mt-24">
+      {/* <Image fill className="object-cover" src={bg} alt="Explore new designs" /> */}
+
+      <div className="relative z-10 text-center ">
+        <h1 className="text-6xl text-primary-50 mb-10 tracking-tight font-normal">
+          Welcome to Akshaya Thread World.
+        </h1>
+        <a
+          href="/products"
+          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore Embroidery designs
+        </a>
+      </div>
+    </main>
+  );
 }
